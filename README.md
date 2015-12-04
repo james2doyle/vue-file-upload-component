d6f58455eeedc042f6f205452cbcfdf044256feb
james2doyle <james2doyle@gmail.com>
2015-12-04 08:54:38 -0800

    add lots of comments. break out pieces

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # vue-file-upload-component
+
 A simple file upload component for Vue.js. Emits events for XHR Upload Progress for nice progress bars.
+
+I came up with the original idea when looking at [this repo](https://github.com/tj/s3.js). I knew I wanted a nice component with upload progress and so I copied some code from that library.
---
+++ js/vue.file-upload.js
@@ -0,0 +1,95 @@
+/* globals FormData, Promise, Vue */
+// define
+var FileUploadComponent = Vue.extend({
+  template: '#file-upload',
+  twoWay: true,
+  props: ['name', 'action', 'accept', 'multiple'],
+  data: function() {
+    return {
+      myFiles: []
+    };
+  },
+  methods: {
+    fileInputClick: function() {
+      // click actually triggers after the file dialog opens
+      this.$dispatch('onFileClick', this.myFiles);
+    },
+    fileInputChange: function() {
+      // get the group of files assigned to this field
+      this.myFiles = document.getElementById(this.name).files;
+      this.$dispatch('onFileChange', this.myFiles);
+    },
+    _onProgress: function(e) {
+      // this is an internal call in XHR to update the progress
+      e.percent = (e.loaded / e.total) * 100;
+      this.$dispatch('onFileProgress', e);
+    },
+    _handleUpload: function(file) {
+      this.$dispatch('beforeFileUpload', file);
+      var form = new FormData();
+      var xhr = new XMLHttpRequest();
+      try {
+        form.append('Content-Type', file.type || 'application/octet-stream');
+        // our request will have the file in the ['file'] key
+        form.append('file', file);
+      } catch (err) {
+        this.$dispatch('onFileError', file, err);
+        return;
+      }
+
+      return new Promise(function(resolve, reject) {
+
+        xhr.upload.addEventListener('progress', this._onProgress, false);
+
+        xhr.onreadystatechange = function() {
+          if (xhr.readyState < 4) {
+            return;
+          }
+          if (xhr.status < 400) {
+            var res = JSON.parse(xhr.responseText);
+            this.$dispatch('onFileUpload', file, res);
+            resolve(file);
+          } else {
+            var err = new Error(xhr.responseText);
+            err.status = xhr.status;
+            err.statusText = xhr.statusText;
+            this.$dispatch('onFileError', file, err);
+            reject(err);
+          }
+        }.bind(this);
+
+        xhr.onerror = function() {
+          var err = new Error(xhr.responseText);
+          err.status = xhr.status;
+          err.statusText = xhr.statusText;
+          this.$dispatch('onFileError', file, err);
+          reject(err);
+        }.bind(this);
+
+        xhr.open('POST', this.action, true);
+        xhr.send(form);
+      }.bind(this));
+    },
+    fileUpload: function() {
+      if(this.myFiles.length > 0) {
+        // a hack to push all the Promises into a new array
+        var arrayOfPromises = Array.prototype.slice.call(this.myFiles, 0).map(function(file) {
+          return this._handleUpload(file);
+        }.bind(this));
+        // wait for everything to finish
+        Promise.all(arrayOfPromises).then(function(allFiles) {
+          this.$dispatch('onAllFilesUploaded', allFiles);
+        }.bind(this)).catch(function(err) {
+          this.$dispatch('onFileError', this.myFiles, err);
+        }.bind(this));
+      } else {
+        // someone tried to upload without adding files
+        var err = new Error("No files to upload for this field");
+        this.$dispatch('onFileError', this.myFiles, err);
+      }
+    }
+  }
+});
+
+// register
+Vue.component('file-upload', FileUploadComponent);
